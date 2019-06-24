85a309b3832dfc4bc295e0e7864b2453828e34f8
Drew Palazzari <drewpalazzari@hotmail.com>
2019-06-23 23:15:31 -0600

    Integrate button menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ButtonMenu from './ButtonMenu'
+import ButtonMenu from './components/ButtonMenu'
+import clone from './helpers/clone'
+import classSetter from './helpers/classSetter'
 import './index.css';
 
 class Bay extends React.Component {
@@ -13,24 +15,75 @@ class Bay extends React.Component {
       ign: false,
       movies: false,
       hackerNews: false,
-      reddit: false
+      reddit: false,
+      weather: false
     }
+  }
+
+  handleClick(i){
+    const buttonValue = i.target.value;
+    let stateObj, bool;
+    switch (buttonValue){
+      case 'top-twitch':
+        stateObj = clone(this.state);
+        bool = stateObj.topTwitch;
+        stateObj.topTwitch = !bool;
+        break;
+      case 'twitch':
+        stateObj = clone(this.state);
+        bool = stateObj.twitch;
+        stateObj.twitch = !bool;
+        break;
+      case 'espn':
+        stateObj = clone(this.state);
+        bool = stateObj.espn;
+        stateObj.espn = !bool;
+        break;
+      case 'hacker':
+        stateObj = clone(this.state);
+        bool = stateObj.hackerNews;
+        stateObj.hackerNews = !bool;
+        break;
+      case 'reddit':
+        stateObj = clone(this.state);
+        bool = stateObj.reddit;
+        stateObj.reddit = !bool;
+        break;
+      case 'ign':
+        stateObj = clone(this.state);
+        bool = stateObj.ign;
+        stateObj.ign = !bool;
+        break;
+      case 'weather':
+        stateObj = clone(this.state);
+        bool = stateObj.weather;
+        stateObj.weather = !bool;
+        break;
+      case 'movies':
+        stateObj = clone(this.state);
+        bool = stateObj.movies;
+        stateObj.movies = !bool;
+        break;
+      default:
+
+    }
+    this.setState(stateObj);
+    classSetter(!bool, i)
   }
 
   render(){
     return (
       <div className='bay'>
-        <div className='button-menu'>
-          <ButtonMenu />
+        <div>
+          <ButtonMenu onClick={(i) => this.handleClick(i)}/>
         </div>
       </div>
     );
   }
 }
 
-ReactDOM.render
-  (
-    <Bay />, 
-    document.getElementById('root'),
-    document.body.classList.add('bay-body')
-  );
+ReactDOM.render(
+  <Bay />, 
+  document.getElementById('root'),
+  document.body.classList.add('bay-body')
+);
