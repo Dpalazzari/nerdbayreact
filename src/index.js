118573ccff522bbd87263b1329ede6701002ec49
Drew Palazzari <drewpalazzari@hotmail.com>
2019-06-23 20:44:33 -0600

    Setup initial react dom component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ButtonMenu from './ButtonMenu'
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Bay extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      twitch: false,
+      topTwitch: false,
+      espn: false,
+      ign: false,
+      movies: false,
+      hackerNews: false,
+      reddit: false
+    }
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render(){
+    return (
+      <div className='bay'>
+        <div className='button-menu'>
+          <ButtonMenu />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render
+  (
+    <Bay />, 
+    document.getElementById('root'),
+    document.body.classList.add('bay-body')
+  );
